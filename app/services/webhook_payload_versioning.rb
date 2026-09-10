63c48561dd1b3e92d243c5782d897c70d6498084
Maya Ortiz <maya.ortiz@atomicmail.io>
2026-09-10 23:51:56 +0300

    Version the partner webhook payload (webhook_payload_versioning.rb)

--- app/services/webhook_payload_versioning.rb
+++ app/services/webhook_payload_versioning.rb
@@ -9,7 +9,7 @@ class WebhookPayloadVersioning
   end
 
   def call
-    return :out_of_area unless crew.covers?(job.site)
+    return :unavailable unless crew.available?(job.window)
 
     job.assign(crew)
   end
